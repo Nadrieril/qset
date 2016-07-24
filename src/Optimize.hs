4c4dba9462d38b83413ecc6c52187ca654b0f557
Nadrieril <nadrieril@gmail.com>
2016-07-24 16:27:39 -0700

    Loops are juste branches with self-reference

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -17,7 +17,6 @@ type Target = (Pat, Lbl)
 
 data Node =
       Go Target
-    | Loop Pat Pat Target
     | Branch Pat Target Target
 
 data GNode = GNode {
@@ -37,7 +36,6 @@ constructInstrGraph :: [Instr] -> G r ()
 constructInstrGraph instrs = forM_ instrs $ \case
     Comment _ -> return ()
     GotoInstr l1 l2 y -> newNode l1 $ Go (MS.fromList y, l2)
-    ForkInstr l1 x l2 y l3 | l1 == l2 -> newNode l1 $ Loop (MS.fromList x) (MS.fromList y) (MS.empty, l3)
     ForkInstr l1 x l2 y l3 -> newNode l1 $ Branch (MS.fromList x) (MS.fromList y, l2) (MS.empty, l3)
 
 newNode :: Lbl -> Node -> G r ()
@@ -50,8 +48,6 @@ deconstructGraph :: Graph -> [SimpInstr]
 deconstructGraph g = uncurry nodeToInstr =<< M.toList g
     where nodeToInstr l1 (node -> n) = case n of
             Go (y, l2) -> [LbldInstr l1 [] l2 (MS.elems y)]
-            Loop x y (z, l3) ->
-                [LbldInstr l1 (MS.elems x) l1 (MS.elems y), LbldInstr l1 [] l3 (MS.elems z)]
             Branch x (y, l2) (z, l3) ->
                 [LbldInstr l1 (MS.elems x) l2 (MS.elems y), LbldInstr l1 [] l3 (MS.elems z)]
 
